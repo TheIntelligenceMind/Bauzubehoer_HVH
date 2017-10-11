0fd8ec13a83b027ef098c70af6bbae3042755894
timhermbecker <t.hermbecker@live.de>
2017-10-11 14:04:24 +0200

    init classes

--- Webshop/src/controller/LoginController.java
+++ Webshop/src/controller/LoginController.java
@@ -1,5 +1,29 @@
 package controller;
 
-public class LoginController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("test");
+		
+		
+		
+		
+		
+	}
+	
+	
 }
