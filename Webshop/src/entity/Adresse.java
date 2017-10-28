6b3723282c2392ff2fa3c02b4f510766abea1a75
juha1996 <julian.hanfgarn@web.de>
2017-10-28 17:49:04 +0200

    filled class adresse

--- Webshop/src/entity/Adresse.java
+++ Webshop/src/entity/Adresse.java
@@ -2,8 +2,64 @@ package entity;
 
 public class Adresse {
 
-	public Adresse(){
+	String straﬂe;
+	String hausnummer;
+	String postleitzahl;
+	String ort;
+	String zusatz;
+	
+	public Adresse(){		
 		
+	}
+	
+	public Adresse init(String piStraﬂe, String piHausnummer, String piPostleitzahl, String piOrt, String piZusatz){		
+		straﬂe = piStraﬂe != null ? piStraﬂe : "";	
+		hausnummer = piHausnummer != null ? piHausnummer : "";	
+		postleitzahl = piPostleitzahl != null ? piPostleitzahl : "";
+		ort = piOrt != null ? piOrt : "";
+		zusatz = piZusatz != null ? piZusatz : "";
+		
+		return this;
+	}
+	
+	public String getStraﬂe() {
+		return straﬂe;
+	}
+
+	public void setStraﬂe(String straﬂe) {
+		this.straﬂe = straﬂe;
+	}
+
+	public String getHausnummer() {
+		return hausnummer;
+	}
+
+	public void setHausnummer(String hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+
+	public String getPostleitzahl() {
+		return postleitzahl;
+	}
+
+	public void setPostleitzahl(String postleitzahl) {
+		this.postleitzahl = postleitzahl;
+	}
+
+	public String getOrt() {
+		return ort;
+	}
+
+	public void setOrt(String ort) {
+		this.ort = ort;
+	}
+
+	public String getZusatz() {
+		return zusatz;
+	}
+
+	public void setZusatz(String zusatz) {
+		this.zusatz = zusatz;
 	}
 	
 }
