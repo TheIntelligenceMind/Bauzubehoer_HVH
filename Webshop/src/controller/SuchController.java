52f04a6fc20ab0d294d621d99ace9677f46a220b
TheIntelligenceMind <t.hermbecker@live.de>
2017-11-14 18:46:12 +0100

    SuchController angepasst, Methodenkommentare hinzugefügt

--- Webshop/src/controller/SuchController.java
+++ Webshop/src/controller/SuchController.java
@@ -21,45 +21,73 @@ import enums.ENUM_ARTIKELKATEGORIE;
 @WebServlet("/suchen")
 public class SuchController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    public SuchController() {
-        super();
-
-    }
-
+	private static final QueryManager queryManager = QueryManager.getInstance();
+	
+    /**
+     * <pre>
+     * <h3>Beschreibung:</h3>
+     * Die Methode verarbeitet alle GET-Schnittstellenaufrufe und gibt 
+     * diese an die doPost() Methode weiter
+     * </pre>
+     * 
+     * @param req
+     * @param resp
+     */
     @Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
 	}
 
+    /**
+     * <pre>
+     * <h3>Beschreibung:</h3>
+     * Die Methode verarbeitet alle POST-Schnittstellenaufrufe und gibt 
+     * entsprechende Werte an die View zurück
+     * </pre>
+     * 
+     * @param req
+     * @param resp
+     */
     @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {		
 		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");	
 		resp.setContentType("text/html");
 		
 		String method = req.getParameter("method");
-			
-		if(method != null){
-			switch(method){
+		
+		// sicherstellen das bei null default eintritt
+		if(method == null){
+			method = "";
+		}
+		
+		switch(method){
 			case "suchKategorie":
 				nachKategorieSuchen(req, resp);
 				break;
 			default:
 				nachBegriffSuchen(req, resp);
 				break;
-			}
-		}else{
-			nachBegriffSuchen(req, resp);
-		}
+		}	
 		
 		rd.forward(req, resp);	
 	}
     
+    /**
+     * <pre>
+     * <h3>Beschreibung:</h3>
+     * Die Methode holt sich das Suchargument aus dem Request-Objekt und 
+     * sucht über den QM alle dazugehörigen Artikel und gibt diese als List 
+     * innerhalb des Request-Objekts an die View weiter
+     * </pre>
+     * 
+     * @param req
+     * @param resp
+     */
     private void nachBegriffSuchen(HttpServletRequest req, HttpServletResponse resp){
     	List<Artikel> artikelliste = new ArrayList<Artikel>();
     	String suchargument = req.getParameter("suchargument");
     	
-		artikelliste = QueryManager.getInstance().searchArtikelByBezeichnung(suchargument);
+		artikelliste = queryManager.searchArtikelByBezeichnung(suchargument);
 		
 		req.setAttribute("artikelliste", artikelliste);
 		
@@ -69,13 +97,24 @@ public class SuchController extends HttpServlet {
 		} 	
     }
     
+    /**
+     * <pre>
+     * <h3>Beschreibung:</h3>
+     * Die Methode holt sich die Artikelkategorie aus dem Request-Objekt und 
+     * sucht über den QM alle dazugehörigen Artikel und gibt diese als List 
+     * innerhalb des Request-Objekts an die View weiter
+     * </pre>
+     * 
+     * @param req
+     * @param resp
+     */
     private void nachKategorieSuchen(HttpServletRequest req, HttpServletResponse resp){
     	List<Artikel> artikelliste = new ArrayList<Artikel>();
     	String strKategorie = req.getParameter("kategorie");
     	
     	if(strKategorie != null){
     		ENUM_ARTIKELKATEGORIE kategorie = ENUM_ARTIKELKATEGORIE.valueOf(strKategorie);
-    		artikelliste = QueryManager.getInstance().searchArtikelByKategorie(kategorie);
+    		artikelliste = queryManager.searchArtikelByKategorie(kategorie);
     		
     		req.setAttribute("artikelliste", artikelliste);
     	}
